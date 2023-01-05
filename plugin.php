fd6114be57e24ec78c8e91217ef9ad08e4df91f8
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-01-05 09:52:46 +0200

    Updated comments

--- plugin.php
+++ plugin.php
@@ -1,12 +1,12 @@
-/**
-    Plugin Name: md5 salt
-    Description: Provide backwards compatibility for the yourls_salt function
-                 to use md5 instead of the default of sha256, so that secure
-                 passwordless API calls continue to function after upgrading
-                 from YOURLS 1.7.4 to 1.9.1
-    Version:     0.1
-    Author:      Ashley Kleynhans
-**/
+<?php
+/*
+Plugin Name: Set HMAC algorithm to MD5
+Plugin URI: https://github.com/ashleykleynhans
+Description: Backwards compatibility for yourls_salt to use md5 instead of sha256 algorithm.
+Version: 1.0
+Author: Ashley Kleynhans
+Author URI: https://github.com/ashleykleynhans
+*/
 
 yourls_add_filter('hmac_algo', 'set_hmac_algo_to_md5');
 
